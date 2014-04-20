d26f39e4ec15c18b87fa3b38d686423868ce8755
Seth Gholson <sethgho@gmail.com>
2014-04-19 23:08:29 -0500

    Initial commit

---
+++ app/src/androidTest/java/co/createch/MetroRappid/test/ExampleTest.java
@@ -0,0 +1,16 @@
+package co.createch.MetroRappid.test;
+
+import android.test.InstrumentationTestCase;
+
+/**
+ * Created by Seth Gholson on 4/19/14.
+ */
+public class ExampleTest extends InstrumentationTestCase {
+    public void testShouldFail() throws Exception {
+        assertTrue(false);
+    }
+
+    public void testShouldSucceed() {
+        assertEquals(1,1);
+    }
+}
