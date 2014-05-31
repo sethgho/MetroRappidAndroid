da741d1e4df6e862b83f8e9c64fed6ea412365f6
Eric Cascketta <eric.cascketta@gmail.com>
2014-05-31 13:16:45 -0500

    added custom application class

---
+++ app/src/main/java/co/createch/MetroRappid/MetroRapidApp.java
@@ -0,0 +1,21 @@
+package co.createch.MetroRappid;
+
+import android.app.Application;
+import android.util.Log;
+
+/**
+ * Created by Caskman on 5/31/2014.
+ */
+public class MetroRapidApp extends Application {
+
+    public static final String TAG = MetroRapidApp.class.getName();
+
+    public MetroRapidApp() {
+
+        Log.d(TAG,"Application context constructor");
+
+    }
+
+
+
+}
