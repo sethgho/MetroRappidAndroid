639c87fb3795ea9466c948f88bf6141f8c2caed6
John Tyree <johntyree@gmail.com>
2014-06-08 03:05:46 -0500

    Cleanup croutons in Activity.onDestroy

--- app/src/main/java/co/createch/MetroRappid/ui/BaseActivity.java
+++ app/src/main/java/co/createch/MetroRappid/ui/BaseActivity.java
@@ -1,9 +1,15 @@
 package co.createch.MetroRappid.ui;
 
 import android.support.v4.app.FragmentActivity;
+import de.keyboardsurfer.android.widget.crouton.Crouton;
 
 /**
  * Created by Seth Gholson on 5/2/14.
  */
 public class BaseActivity extends FragmentActivity {
+
+    @Override
+    protected void onDestroy() {
+        Crouton.cancelAllCroutons();
+    }
 }
