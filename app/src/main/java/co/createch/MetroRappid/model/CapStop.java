58f5dc2bbead0e22bd36bed6768885c59ea28845
John Tyree <johntyree@gmail.com>
2014-06-08 20:20:44 -0500

    Remove CapStop.direction

--- app/src/main/java/co/createch/MetroRappid/model/CapStop.java
+++ app/src/main/java/co/createch/MetroRappid/model/CapStop.java
@@ -90,7 +90,6 @@ public class CapStop implements Comparable, Parcelable { //extend Maps marker?
         dest.writeString(this.name);
         dest.writeString(this.description);
         dest.writeInt(this.stopSequence);
-        dest.writeInt(this.direction == null ? -1 : this.direction.ordinal());
         dest.writeString(this.headsign);
         dest.writeFloat(this.distance);
         dest.writeByte(isDistanceKnown ? (byte) 1 : (byte) 0);
@@ -108,8 +107,6 @@ public class CapStop implements Comparable, Parcelable { //extend Maps marker?
         this.name = in.readString();
         this.description = in.readString();
         this.stopSequence = in.readInt();
-        int tmpDirection = in.readInt();
-        this.direction = tmpDirection == -1 ? null : RouteDirection.values()[tmpDirection];
         this.headsign = in.readString();
         this.distance = in.readFloat();
         this.isDistanceKnown = in.readByte() != 0;
